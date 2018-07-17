bfbc6162decdfedab0b58e226aac371c286dc85e
dee <dee@adadevelopersacademy.org>
2018-07-17 13:58:58 -0700

    tests/impl for drawing 10 tiles at a time

--- specs/adagrams_spec.rb
+++ specs/adagrams_spec.rb
@@ -8,9 +8,21 @@ require_relative '../lib/adagrams'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Adagrams' do
-  describe 'score' do
-    it 'has a running test' do
-      true.must_equal true
+  describe 'draw_letters method' do
+    it 'draws ten letters from the letter pool' do
+      drawn_letters = draw_letters
+      drawn_letters.size.must_equal 10
+    end
+
+    it 'returns an array, and each item is a single-letter string' do
+      drawn_letters = draw_letters
+      drawn_letters.size.must_equal 10
+
+      drawn_letters.must_be_instance_of Array
+      drawn_letters.each do |letter|
+        letter.must_be_instance_of String
+        letter.length.must_equal 1
+      end
     end
   end
 end
