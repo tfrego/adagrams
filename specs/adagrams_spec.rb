4efd427fd306491bfb36ed1a07e992095a049db9
CheezItMan <chris@adadevelopersacademy.org>
2018-08-13 08:07:20 -0700

    updated to use expect

--- specs/adagrams_spec.rb
+++ specs/adagrams_spec.rb
@@ -11,17 +11,17 @@ describe 'Adagrams' do
   describe 'draw_letters method' do
     it 'draws ten letters from the letter pool' do
       drawn_letters = draw_letters
-      drawn_letters.size.must_equal 10
+      expect(drawn_letters.size).must_equal 10
     end
 
     it 'returns an array, and each item is a single-letter string' do
       drawn_letters = draw_letters
-      drawn_letters.size.must_equal 10
+      expect(drawn_letters.size).must_equal 10
 
-      drawn_letters.must_be_instance_of Array
+      expect(drawn_letters).must_be_instance_of Array
       drawn_letters.each do |letter|
-        letter.must_be_instance_of String
-        letter.length.must_equal 1
+        expect(letter).must_be_instance_of String
+        expect(letter.length).must_equal 1
       end
     end
   end
@@ -43,7 +43,7 @@ describe 'Adagrams' do
 
       is_valid = uses_available_letters? test_word, drawn_letters
 
-      is_valid.must_equal false
+      expect(is_valid).must_equal false
     end
 
     it 'returns false word contains repeated letters more than in the drawn letters' do
@@ -52,32 +52,32 @@ describe 'Adagrams' do
 
       is_valid = uses_available_letters? test_word, drawn_letters
 
-      is_valid.must_equal false
+      expect(is_valid).must_equal false
     end
 
   end
 
   describe 'score_word method' do
     it 'returns an accurate numerical score according to the score chart' do
-      score_word("A").must_equal 1
-      score_word("DOG").must_equal 5
-      score_word("WHIMSY").must_equal 17
+      expect(score_word("A")).must_equal 1
+      expect(score_word("DOG")).must_equal 5
+      expect(score_word("WHIMSY")).must_equal 17
     end
 
     it 'returns a score regardless of input case' do
-      score_word("a").must_equal 1
-      score_word("dog").must_equal 5
-      score_word("wHiMsY").must_equal 17
+      expect(score_word("a")).must_equal 1
+      expect(score_word("dog")).must_equal 5
+      expect(score_word("wHiMsY")).must_equal 17
     end
 
     it 'returns a score of 0 if given an empty input' do
-      score_word("").must_equal 0
+      expect(score_word("")).must_equal 0
     end
 
     it 'adds an extra 8 points if the word is 7 or more characters long' do
-      score_word("XXXXXXX").must_equal 64
-      score_word("XXXXXXXX").must_equal 72
-      score_word("XXXXXXXXX").must_equal 80
+      expect(score_word("XXXXXXX")).must_equal 64
+      expect(score_word("XXXXXXXX")).must_equal 72
+      expect(score_word("XXXXXXXXX")).must_equal 80
     end
   end
 
@@ -86,16 +86,16 @@ describe 'Adagrams' do
       words = ['X', 'XX', 'XXX', 'XXXX']
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'XXXX'
-      best_word[:score].must_equal 32
+      expect(best_word[:word]).must_equal 'XXXX'
+      expect(best_word[:score]).must_equal 32
     end
 
     it 'accurately finds best scoring word even if not sorted' do
       words = ['XXX', 'XXXX', 'XX', 'X']
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'XXXX'
-      best_word[:score].must_equal 32
+      expect(best_word[:word]).must_equal 'XXXX'
+      expect(best_word[:score]).must_equal 32
     end
 
     it 'in case of tied score, prefers the word with fewer letters' do
@@ -103,13 +103,13 @@ describe 'Adagrams' do
       words = ['MMMM', 'WWW']
 
       # verify both have a score of 12
-      score_word(words.first).must_equal 12
-      score_word(words.last).must_equal 12
+      expect(score_word(words.first)).must_equal 12
+      expect(score_word(words.last)).must_equal 12
 
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'WWW'
-      best_word[:score].must_equal 12
+      expect(best_word[:word]).must_equal 'WWW'
+      expect(best_word[:score]).must_equal 12
     end
 
     it 'in case of tied score, prefers the word with fewer letters regardless of order' do
@@ -117,13 +117,13 @@ describe 'Adagrams' do
       words = ['WWW', 'MMMM']
 
       # verify both have a score of 12
-      score_word(words.first).must_equal 12
-      score_word(words.last).must_equal 12
+      expect(score_word(words.first)).must_equal 12
+      expect(score_word(words.last)).must_equal 12
 
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'WWW'
-      best_word[:score].must_equal 12
+      expect(best_word[:word]).must_equal 'WWW'
+      expect(best_word[:score]).must_equal 12
     end
 
     it 'in case of tied score, prefers most the word with 10 letters' do
@@ -131,13 +131,13 @@ describe 'Adagrams' do
       words = ['AAAAAAAAAA', 'BBBBBB']
 
       # verify both have a score of 10
-      score_word(words.first).must_equal 18
-      score_word(words.last).must_equal 18
+      expect(score_word(words.first)).must_equal 18
+      expect(score_word(words.last)).must_equal 18
 
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'AAAAAAAAAA'
-      best_word[:score].must_equal 18
+      expect(best_word[:word]).must_equal 'AAAAAAAAAA'
+      expect(best_word[:score]).must_equal 18
     end
 
     it 'in case of tied score, prefers most the word with 10 letters regardless of order' do
@@ -145,13 +145,13 @@ describe 'Adagrams' do
       words = ['BBBBBB', 'AAAAAAAAAA']
 
       # verify both have a score of 10
-      score_word(words.first).must_equal 18
-      score_word(words.last).must_equal 18
+      expect(score_word(words.first)).must_equal 18
+      expect(score_word(words.last)).must_equal 18
 
       best_word = highest_score_from words
 
-      best_word[:word].must_equal 'AAAAAAAAAA'
-      best_word[:score].must_equal 18
+      expect(best_word[:word]).must_equal 'AAAAAAAAAA'
+      expect(best_word[:score]).must_equal 18
     end
 
     it 'in case of tied score and same length words, prefers the first word' do
@@ -159,15 +159,13 @@ describe 'Adagrams' do
       words = ['AAAAAAAAAA', 'EEEEEEEEEE']
 
       # verify both have a score of 10
-      score_word(words.first).must_equal 18
-      score_word(words.last).must_equal 18
+      expect(score_word(words.first)).must_equal 18
+      expect(score_word(words.last)).must_equal 18
 
       best_word = highest_score_from words
 
-      best_word[:word].must_equal words.first
-      best_word[:score].must_equal 18
+      expect(best_word[:word]).must_equal words.first
+      expect(best_word[:score]).must_equal 18
     end
-
   end
-
 end
