0717e479c40e184e6b63855e5d99c3e672ea6569
Trang <trang.frego@gmail.com>
2018-08-15 16:53:24 -0700

    Refactored code and revised comments

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,59 +1,31 @@
 
-
-letter_pool = {
-  :A => 9,
-  :B => 2,
-  :C => 2,
-  :D => 4,
-  :E => 12,
-  :F => 2,
-  :G => 3,
-  :H => 2,
-  :I => 9,
-  :J => 1,
-  :K => 1,
-  :L => 4,
-  :M => 2,
-  :N => 6,
-  :O => 8,
-  :P => 2,
-  :Q => 1,
-  :R => 6,
-  :S => 4,
-  :T => 6,
-  :U => 4,
-  :V => 2,
-  :W => 2,
-  :X => 1,
-  :Y => 2,
-  :Z => 1
-}
-
-# Method to draw letters and return an array of 10 strings
+# Draws letters and returns an array of 10 strings
 def draw_letters
   # Empty array to hold drawn letters
   drawn_letters = []
-  # Array of all the letters available to be drawn from pool
+  # Array of all the letters available to be drawn
   available_letters = %w(A A A A A A A A A B B C C D D D D E E E E E E E E E E E E
   F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P
   Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z)
-  # randomly draw letter from pool
+  # Randomly draw letter
   10.times do
+    # Assign random letter
     letter = available_letters.sample
-    # push random letter
+    # Push random letter
     drawn_letters << letter
-    # delete drawn letter from available_letters array
+    # Delete drawn letter from available_letters
     available_letters.delete_at(available_letters.index(letter))
   end
   return drawn_letters
 end
 
-draw_letters
-
+# Validates input against letters_in_hand
 def uses_available_letters?(input, letters_in_hand)
-input.upcase!
-letter_results = []
-  # check if letter is in letters_in_hand with right quantities
+  # Capitalizing input
+  input.upcase!
+  # Empty array to hold results
+  letter_results = []
+  # Validating quantitiy
   input.chars.each do |letter|
     if input.count(letter) > letters_in_hand.count(letter)
       letter_results << false
@@ -61,14 +33,18 @@ letter_results = []
       letter_results << true
     end
   end
+  # Returns true if input quantity is valid
   return letter_results.all?(true)
 end
 
-
+# Scores word
 def score_word(word)
+  # Starting score at 0
   score = 0
+  # Capitalizing word
   word.upcase!
 
+  # Updating score based on letter
   word.chars.each do |letter|
     case letter
     when 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'
@@ -87,47 +63,46 @@ def score_word(word)
       score += 10
     end
   end
-
-  if word.chars.length.between?(7,10)
-    score += 8
-  end
-
+  # Adding bonus points based on word length
+  score += 8 if word.chars.length.between?(7,10)
   return score
 end
 
-# method to find highest scoring word
-def highest_score_from_words(words)
-  word_scores = words.map do |word|
-    {:word => word, :score => score_word(word)}
-  end
+# Returning winning word/score
+def highest_score_from(words)
+  # Makes an array of words and associated score
+  word_scores = words.map { |word| {:word => word, :score => score_word(word)} }
 
-  # Determine the winning score
-    # word_score array - find top score
-    top_score = word_scores.max_by{ |k| k[:score] } [:score]
-  # Select hashes in word_scores that have winning score
-    potential_winners = word_scores.select { |h| h[:score] == top_score }
+  # Assignign the top score value
+  top_score = word_scores.max_by{ |k| k[:score] } [:score]
+
+  # Selecting hashes in word_scores that have winning score
+  potential_winners = word_scores.select { |h| h[:score] == top_score }
+
   # Checking length of potential winners array
-
   if potential_winners.length > 1
+
     # If greater than one, there's a tie
     potential_winners.each do |potential_winner|
-      # Adding word lengths to potential winners
+      # Adding word lengths to potential winner hash
       potential_winner[:word_length] = potential_winner[:word].length
     end
-    # Select if length is 10, return first as winner
+
+    # Creating an array if word length is 10
     length_of_10 = potential_winners.select { |h| h[:word_length] == 10 }
+
+    # Checking if length_of_10 is not empty
     if length_of_10.length > 0
-      winner = { :word => length_of_10[0][:word], :score => length_of_10[0][:score]}
+      # Returning first word/score in the length_of_10 array
+      return { :word => length_of_10[0][:word], :score => length_of_10[0][:score]}
     else
-      # No tens, pick fewest letters
-      winner = potential_winners.min_by { |potential_winner| potential_winner[:word_length]}
-      winner = { :word => winner[:word], :score => winner[:score] }
-      # If all the same length pick the first one
+      # Returning first word/score and rejecting word_length key/value pair
+      return potential_winners.min_by { |potential_winner| potential_winner[:word_length]}\
+      .reject! { |k| k == :word_length
+       }
     end
   else
-    # If there's just one word, return that hash
+    # Returning the value of potential_winners if there is no tie
     return potential_winners[0]
   end
 end
-
-highest_score_from_words(["aaaa", "aeee", 'da', 'daa'])
